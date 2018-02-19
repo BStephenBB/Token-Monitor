6f232b22d1f1cbad40d508340834d316169dd7b7
StephenBB <36211879+BStephenBB@users.noreply.github.com>
2018-02-19 15:38:54 -0800

    Token monitor code and example environment variables
    Make sure to add your own information and rename sample_env_variables to just env_variables for the code to work.

---
+++ sample_env_variables.py
@@ -0,0 +1,5 @@
+your_API = 'your API here'
+from_address = "from email address here"
+email_password = "from email password here"
+to_addresses = ["1st email to send to", "2nd to send to", "3rd to send to", "etc"]
+
